aa6648c418259d007ca351790c92cfd706820c6f
Jean Faria <111452998+jeejinf@users.noreply.github.com>
2023-09-22 10:37:29 -0300

    Adicionado a criação de 3 projetos para a Gabi testar o front, e configurações de dialeto + driver do MySQL

--- NINETECH/src/main/java/com/example/fatec/ninetech/NinetechApplication.java
+++ NINETECH/src/main/java/com/example/fatec/ninetech/NinetechApplication.java
@@ -1,5 +1,6 @@
 package com.example.fatec.ninetech;
 
+import java.time.LocalDate;
 import java.util.HashMap;
 
 import org.springframework.boot.SpringApplication;
@@ -23,15 +24,13 @@ public class NinetechApplication {
 		
 		Map<String, Object> configuracao = new HashMap<>();
 
-		configuracao.put("server.port", "5000");
-
+		configuracao.put("server.port", "5000"); // porta do backend
 		configuracao.put("spring.datasource.url", "jdbc:mysql://localhost:3306/ninetech"); // caminho da conexão
-		
 		configuracao.put("spring.datasource.username", "root"); // usuario
 		configuracao.put("spring.datasource.password", "123456"); // senha
-		
-		//configuracao.put("spring.jpa.show-sql", "true"); // mostrar comandos
-		configuracao.put("spring.jpa.hibernate.ddl-auto", "update"); // criar editar
+        configuracao.put("spring.datasource.driver-class-name", "com.mysql.cj.jdbc.Driver"); // driver mysql
+        configuracao.put("spring.jpa.hibernate.ddl-auto", "update"); // criar - atualizar
+        configuracao.put("spring.jpa.properties.hibernate.dialect", "org.hibernate.dialect.MySQLDialect"); // dialeto mysql
 		
 		SpringApplication app = new SpringApplication(NinetechApplication.class);
 		app.setDefaultProperties(configuracao);
@@ -61,6 +60,28 @@ public class NinetechApplication {
 	        LiderDeProjeto liderDeProjeto2 = new LiderDeProjeto();
 	        liderDeProjeto2.setNome("Líder de Projeto 2");
 	        servicoLiderDeProjeto.criarLiderDeProjeto(liderDeProjeto2);
+	        
+	        Projeto projeto = new Projeto();
+	        projeto.setNome("Carro");
+	        projeto.setData_inicio(LocalDate.of(2023, 9, 14));
+	        projeto.setData_final(LocalDate.of(2024, 9, 14)); 
+	        projeto.setEngenheiroChefe(engenheiroChefe);
+	        servicoProjeto.criarProjeto(projeto);
+
+	        Projeto projeto2 = new Projeto();
+	        projeto2.setNome("Barco");
+	        projeto2.setData_inicio(LocalDate.of(2023, 9, 14));
+	        projeto2.setData_final(LocalDate.of(2024, 6, 25)); 
+	        projeto2.setEngenheiroChefe(engenheiroChefe);
+	        servicoProjeto.criarProjeto(projeto2);
+
+	        Projeto projeto3 = new Projeto();
+	        projeto3.setNome("Avião");
+	        projeto3.setData_inicio(LocalDate.of(2023, 9, 14));
+	        projeto3.setData_final(LocalDate.of(2025, 6, 25)); 
+	        projeto3.setEngenheiroChefe(engenheiroChefe);
+	        servicoProjeto.criarProjeto(projeto3);
+
 		} 
 	}
 }
