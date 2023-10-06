850118e158e2e71ff8b54e737b0b7ee7b08413c0
Igor Pereira <igorpereir28@hotmail.com>
2023-10-06 10:27:57 -0300

    Refatoração da iniciação do spring, diretamente na porta 5000

--- NINETECH/src/main/java/com/example/fatec/ninetech/NinetechApplication.java
+++ NINETECH/src/main/java/com/example/fatec/ninetech/NinetechApplication.java
@@ -22,26 +22,9 @@ public class NinetechApplication {
 
 	public static void main(String[] args) {
 		ApplicationContext context = SpringApplication.run(NinetechApplication.class, args);
-		
-		Map<String, Object> configuracao = new HashMap<>();
-
-		configuracao.put("server.port", "5000"); // porta do backend
-		configuracao.put("spring.main.lazy-initialization","false");
-		configuracao.put("spring.datasource.initialization-mode", "always");
-		configuracao.put("spring.jpa.hibernate.ddl-auto", "update");
-		configuracao.put("spring.datasource.url", "jdbc:mysql://localhost:3306/ninetech?createDatabaseIfNotExist=true"); // caminho da conexão
-		configuracao.put("spring.datasource.username", "root"); // usuario
-		configuracao.put("spring.datasource.password", "123456"); // senha
-        configuracao.put("spring.datasource.driver-class-name", "com.mysql.cj.jdbc.Driver"); // driver mysql
-        configuracao.put("spring.jpa.properties.hibernate.dialect", "org.hibernate.dialect.MySQLDialect"); // dialeto mysql
-		
-		SpringApplication app = new SpringApplication(NinetechApplication.class);
-		app.setDefaultProperties(configuracao);
-		app.run(args);
-		
+				
 		
 		//!!! Setando alguns usuarios
-        // Obtendo o serviço EngenheiroChefeServico do contexto Spring
 		EngenheiroChefeServico servicoEngenheiroChefe = context.getBean(EngenheiroChefeServico.class);
 		LiderDeProjetoServico servicoLiderDeProjeto = context.getBean(LiderDeProjetoServico.class);
 		ProjetoServico servicoProjeto = context.getBean(ProjetoServico.class);
